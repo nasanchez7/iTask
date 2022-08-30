2d4dad681499f58d907a15c4dc10436df4b8c17f
nasanchez7 <nadirblanco02@gmail.com>
2022-08-30 19:49:31 -0300

    advance in the logic

--- js/app.js
+++ js/app.js
@@ -1,11 +1,20 @@
 "use strict";
+//Obtener fecha de hoy
+const getHour = () => {
+    const date = new Date;
+    const hour = date.getHours();
+    const minutes = date.getMinutes();
+    return hour + ":" + minutes;
+};
 const getDay = () => {
     const date = new Date;
     const day = date.getDay();
     const days = ["Domingo", "Lunes", "Martes", "Miércoles", "Jueves", "Viernes", "Sábado"];
     const dayString = days[day];
     const dayHtml = document.getElementById("date");
+    const dayDate = dayString + " " + date.getDate();
     dayHtml.innerHTML = dayString + " " + date.getDate();
+    return dayDate;
 };
 const getMonth = () => {
     const date = new Date;
@@ -17,4 +26,41 @@ const getMonth = () => {
 };
 getDay();
 getMonth();
+getHour();
+//Logica nueva tarea
+class Task {
+    constructor(info, date) {
+        this.info = info;
+        this.date = date;
+    }
+}
+const tasks = [];
+const btnTask = document.getElementById("btnTask");
+const newTask = () => {
+    const date = new Date;
+    const hour = date.getHours();
+    const minutes = date.getMinutes();
+    const time = hour + ":" + minutes;
+    const infoTask = document.getElementById("taskContent");
+    const newTask = new Task(infoTask.value, time);
+    tasks.push(newTask);
+    console.log(tasks);
+    const listTask = document.getElementById("listTask");
+    listTask.innerHTML = "";
+    tasks.forEach((task, index) => {
+        const newTask = document.createElement("div");
+        newTask.className = "task";
+        newTask.innerHTML = `
+        <div class="infoTask">
+            <h5> ${task.date} </h5>
+            <p> ${task.info} </p>
+        </div>
+        <div class="deleteTask">
+            <i class='bx bxs-trash' id="deleteTask${index}"></i>
+        </div>
+        `;
+        listTask.appendChild(newTask);
+    });
+};
+btnTask.addEventListener("click", newTask);
 //# sourceMappingURL=app.js.map
--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,17 @@
-const getDay = ()=> {
+//Obtener fecha de hoy
+
+const getHour = (): string => {
+
+    const date: Date = new Date;
+
+    const hour: number = date.getHours();
+    const minutes: number = date.getMinutes();
+
+    return hour + ":" + minutes
+    
+}
+
+const getDay = (): string=> {
     const date: Date = new Date;
 
     const day = date.getDay();
@@ -8,7 +21,12 @@ const getDay = ()=> {
     const dayString: string = days[day];
 
     const dayHtml: any = document.getElementById("date");
+
+    const dayDate: string = dayString + " " + date.getDate();
+
     dayHtml.innerHTML = dayString + " " + date.getDate();
+
+    return dayDate
 }
 
 const getMonth = ()=> {
@@ -25,10 +43,61 @@ const getMonth = ()=> {
 
 getDay();
 getMonth();
+getHour();
 
 
+//Logica nueva tarea
+
+class Task {
+    info: string
+    date: string
+
+    constructor(info: string, date: string) {
+        this.info = info;
+        this.date = date;
+    }
+}
 
 
+const tasks: Task[] = [];
+
+const btnTask: any = document.getElementById("btnTask");
+
+const newTask = () => {
+
+    const date: Date = new Date;
+    const hour: number = date.getHours();
+    const minutes: number = date.getMinutes();
+
+    const time: string = hour + ":" + minutes;
+    const infoTask: any = document.getElementById("taskContent");
+
+    const newTask: Task = new Task(infoTask.value, time);
+    tasks.push(newTask);
+
+    console.log(tasks);
+
+    const listTask: any = document.getElementById("listTask");
+    listTask.innerHTML = "";
+
+    tasks.forEach((task, index) => {
+        const newTask: any = document.createElement("div");
+        newTask.className = "task";
+        newTask.innerHTML = `
+        <div class="infoTask">
+            <h5> ${task.date} </h5>
+            <p> ${task.info} </p>
+        </div>
+        <div class="deleteTask">
+            <i class='bx bxs-trash' id="deleteTask${index}"></i>
+        </div>
+        `
+        listTask.appendChild(newTask);
+    })
+
+}
+
+btnTask.addEventListener("click", newTask);
 
 
 
