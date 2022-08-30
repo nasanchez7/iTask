5d969b08449584c4d56b8afa49326f2945551152
nasanchez7 <nadirblanco02@gmail.com>
2022-08-30 17:15:46 -0300

    styling

--- js/app.js
+++ js/app.js
@@ -1,4 +1,20 @@
 "use strict";
-console.log("hola nadir");
-console.log("chau nadir");
+const getDay = () => {
+    const date = new Date;
+    const day = date.getDay();
+    const days = ["Domingo", "Lunes", "Martes", "Miércoles", "Jueves", "Viernes", "Sábado"];
+    const dayString = days[day];
+    const dayHtml = document.getElementById("date");
+    dayHtml.innerHTML = dayString + " " + date.getDate();
+};
+const getMonth = () => {
+    const date = new Date;
+    const monthNumber = date.getMonth();
+    const months = ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"];
+    const monthString = months[monthNumber];
+    const monthHtml = document.getElementById("month");
+    monthHtml.innerHTML = monthString;
+};
+getDay();
+getMonth();
 //# sourceMappingURL=app.js.map
--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,39 @@
+const getDay = ()=> {
+    const date: Date = new Date;
+
+    const day = date.getDay();
+
+    const days: string[]= ["Domingo","Lunes","Martes","Miércoles","Jueves","Viernes","Sábado"];
+
+    const dayString: string = days[day];
+
+    const dayHtml: any = document.getElementById("date");
+    dayHtml.innerHTML = dayString + " " + date.getDate();
+}
+
+const getMonth = ()=> {
+    const date: Date = new Date;
+
+    const monthNumber = date.getMonth();
+    const months: string[] = ["Enero","Febrero","Marzo","Abril","Mayo","Junio","Julio","Agosto","Septiembre","Octubre","Noviembre","Diciembre"];
+    
+    const monthString: string = months[monthNumber]; 
+
+    const monthHtml: any = document.getElementById("month");
+    monthHtml.innerHTML = monthString;
+}
+
+getDay();
+getMonth();
+
+
+
+
+
+
+
+
+
+
+
 
